d20d1ac03898340b62155783ccc8051aad4c984a
Tinny <tinny.nguyen@adrenalinmedia.com.au>
2018-07-20 17:35:56 +1000

    testing modules

---
+++ src/amodule.lua
@@ -0,0 +1,9 @@
+local _M = {}
+
+function _M.aprint(text)
+
+  print(text)
+
+end
+
+return _M
---
+++ src/init.lua
@@ -0,0 +1,5 @@
+local aa = require "amodule"
+local bb = require "galconmodslib"
+
+aa.aprint('xxx')
+bb.test()
